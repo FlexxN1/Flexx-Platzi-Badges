a0418a5f4bb173930055cd147926ae16dc93c099
Flexx <juandavidx40@gmail.com>
2023-01-04 21:22:23 -0500

    Updates

---
+++ precache-manifest.76f7fbe3e8c48412d7a0016a9b6a42f0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "285fbf8ab2073c153078",
+    "url": "/Flexx-Platzi-Badges/static/css/main.8066e82e.chunk.css"
+  },
+  {
+    "revision": "285fbf8ab2073c153078",
+    "url": "/Flexx-Platzi-Badges/static/js/main.285fbf8a.chunk.js"
+  },
+  {
+    "revision": "c0be8d727d9e0cb7b0c4",
+    "url": "/Flexx-Platzi-Badges/static/js/runtime~main.c0be8d72.js"
+  },
+  {
+    "revision": "8af5565a1a0fb0683036",
+    "url": "/Flexx-Platzi-Badges/static/css/2.a1208501.chunk.css"
+  },
+  {
+    "revision": "8af5565a1a0fb0683036",
+    "url": "/Flexx-Platzi-Badges/static/js/2.8af5565a.chunk.js"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/Flexx-Platzi-Badges/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/Flexx-Platzi-Badges/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/Flexx-Platzi-Badges/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/Flexx-Platzi-Badges/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/Flexx-Platzi-Badges/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "a3a8a1d04064acc9a1990e3bd198375c",
+    "url": "/Flexx-Platzi-Badges/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Flexx-Platzi-Badges/precache-manifest.76f7fbe3e8c48412d7a0016a9b6a42f0.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Flexx-Platzi-Badges/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
